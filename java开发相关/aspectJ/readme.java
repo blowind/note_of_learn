947f479ccfc129480449827770ec362c98942c7c
blowind <zhangxuefengcs@163.com>
2018-06-20 08:03:56 +0800

    添加aspectJ知识点

---
+++ java开发相关/aspectJ/readme.java
@@ -0,0 +1,165 @@
+
+/******************               Pointcut举例                  ******************/
+call(void Point.setX(int)   //  定义对Point类的一参数setX函数的调用为切点
+call(void Point.setx(int)) || call(void Line.setP1(Point)  //  使用 && , || , ! 组合切点，各切点可以属于不同的类
+
+/* 自定义一个切点，可以使用move()表示一个切点 */
+pointcut move() :
+	call(void FigureElement.setXY(int, int) ||   //  此处是接口函数
+	call(void Point.setX(int))              ||
+	call(void Line.setP1(int));
+	
+/*  切点的模糊匹配定义  */	
+call(void Figure.make*(..))         //  定义一个切点，Figure类/接口中以make开头的任意参数的方法，其返回值是void
+call(public * Figure.*(..))         //  定义一个切点，指向Figure类/接口的所有公共方法
+cflow(move())                       //  使用cflow定义切点，指向所有move切点开始调用和返回之间的点
+
+/******************               Advice举例                  ******************/
+// before原语定义move()调用前的建言
+before(): move() {
+	Sysout.out.println("about to move");
+}
+// after原语定义move()调用后的建言，包括正常return和异常exception返回，此处使用正常return
+after() returning: move() {
+    System.out.println("just successfully moved");
+}
+
+// 建言中使用参数，参数有切点传入，可以分别使用this, target, args三个原语传入，此处使用了两种
+after(FigureElement fe, int x, int y) returning:
+        call(void FigureElement.setXY(int, int))
+        && target(fe)
+        && args(x, y) {
+    System.out.println(fe + " moved to (" + x + ", " + y + ")");
+}
+
+//  上述建言的另一种写法，将Pointcut先提炼简化后再使用
+pointcut setXY(FigureElement fe, int x, int y):
+    call(void FigureElement.setXY(int, int))
+    && target(fe)
+    && args(x, y);
+after(FigureElement fe, int x, int y) returning: setXY(fe, x, y) {
+    System.out.println(fe + " moved to (" + x + ", " + y + ").");
+}
+
+// 使用thisJoinPoint特殊变量
+aspect SimpleTracing {
+    pointcut tracedCall():
+        call(void FigureElement.draw(GraphicsContext));
+
+    before(): tracedCall() {
+        System.out.println("Entering: " + thisJoinPoint);  // 输出： Entering: call(void FigureElement.draw(GraphicsContext))
+    }
+}
+
+/******************               Inter-type declarations内部变量声明                  ******************/
+// 给已有类的新增内部变量，此处给Point新增observers变量，该变量用于存储监控Point变化的Screen对象
+// 这种监控关系完全不需要在Point类和Screen类里面编写相关处理代码，所有具体关联处理全部放入本Aspect定义
+aspect PointObserving {
+    private List<Screen> Point.observers = new List<Screen>();  // private，所以仅对PointObserving切面可见
+
+    public static void addObserver(Point p, Screen s) {
+        p.observers.add(s);
+    }
+    public static void removeObserver(Point p, Screen s) {
+        p.observers.remove(s);
+    }
+	
+	pointcut changes(Point p): target(p) && call(void Point.set*(int));  // 定义切点，所有Point对象的一参数set方法
+
+    after(Point p): changes(p) {          // 定义建言，即在set方法返回时，调用切面的updateObserver方法
+        Iterator iter = p.observers.iterator();
+        while ( iter.hasNext() ) {
+            updateObserver(p, (Screen)iter.next());
+        }
+    }
+	
+	static void updateObserver(Point p, Screen s) {
+        s.display(p);
+    }
+}
+
+/******************               Aspects定义                  ******************/
+// aspect切面的实例化都由AspectJ处理，默认是单例的，因此可以有局部变量和非静态函数的使用
+aspect Logging {
+    OutputStream logStream = System.err;
+
+    before(): move() {
+        logStream.println("about to move");
+    }
+}
+
+// 切面的性能调研用法
+aspect SetsInRotateCounting {
+    int rotateCount = 0;
+    int setCount = 0;
+
+	// 计算Line对象中rotate方法被调用的次数
+    before(): call(void Line.rotate(double)) {
+        rotateCount++;
+    }
+
+	// 计算Line.rotate方法内部Point对象的set*方法被调用的次数
+    before(): call(void Point.set*(int))
+              && cflow(call(void Line.rotate(double))) {
+        setCount++;
+    }
+}
+
+//  切面的输入检查用法(同理可以进行输出检查)，可以精确控制不同类的相关方法
+aspect PointBoundsChecking {
+
+    pointcut setX(int x):
+        (call(void FigureElement.setXY(int, int)) && args(x, *))
+        || (call(void Point.setX(int)) && args(x));
+
+    pointcut setY(int y):
+        (call(void FigureElement.setXY(int, int)) && args(*, y))
+        || (call(void Point.setY(int)) && args(y));
+
+    before(int x): setX(x) {
+        if ( x < MIN_X || x > MAX_X )
+            throw new IllegalArgumentException("x is out of bounds.");
+    }
+
+    before(int y): setY(y) {
+        if ( y < MIN_Y || y > MAX_Y )
+            throw new IllegalArgumentException("y is out of bounds.");
+    }
+}
+
+//  切面的异常检测用法，用于识别不合理的函数调用
+aspect RegistrationProtection {
+
+    pointcut register(): call(void Registry.register(FigureElement));  // FigureElement对象被注册的调用点
+	// 使用withincode原语指向FigureElement类make工厂方法内部的所有地方为切点
+    pointcut canRegister(): withincode(static * FigureElement.make*(..));  // 
+
+    before(): register() && !canRegister() {
+        throw new IllegalAccessException("Illegal call " + thisJoinPoint);
+    }
+}
+// 同上，不是通过运行时抛出异常，而是通过declare原语指明在编译期抛出error，这种方式在static方法调用的切点里可用
+aspect RegistrationProtection {
+
+    pointcut register(): call(void Registry.register(FigureElement));
+    pointcut canRegister(): withincode(static * FigureElement.make*(..));
+
+    declare error: register() && !canRegister(): "Illegal call"
+}
+
+//  捕获com.bigboxco包下面所有public方法抛给调用者的所有Error，并记录日志
+aspect PublicErrorLogging {
+    Log log = new Log();
+
+    pointcut publicMethodCall():
+        call(public * com.bigboxco.*.*(..)); // com.bigboxco包下面的所有public方法
+	/*
+    after() throwing (Error e): publicMethodCall() { // 在切点抛出Error之后
+        log.write(e);
+    } */
+	// 前述after中，如果本地public方法调用本地public方法，则会捕获并记录两次日志
+	after() throwing (Error e):
+        publicMethodCall() && !cflow(publicMethodCall()) {  //  排除本地方法的内部互相调用时多次记录日志的情况
+		log.write(e);
+	}
+}
