bba422b973ba294093106cd2d4cdb0a82a8d486d
blowind <zhangxuefengcs@163.com>
2018-06-20 21:15:11 +0800

    补充切面知识

--- java开发相关/aspectJ/readme.java
+++ java开发相关/aspectJ/readme.java
@@ -1,7 +1,48 @@
 
-/******************               Pointcut举例                  ******************/
+                         /******************               Pointcut举例                  ******************/
+
+/*
+pointcut 中的join points主要包括如下几种: 
+	1、方法调用：                           call(void Point.setX(int))
+	2、方法执行：                           execution(void Point.setX(int))
+	3、对象初始化：
+	4、构造函数执行：                       call(*.new(int, int))
+	5、域变量引用：
+	6、异常处理：                           handler(ArrayOutOfBoundsException)
+	7、当前运行的对象属于SomeType类         this(SomeType)
+	8、被调用的对象属于SomeType类           target(SomeType)
+	9、执行的代码位于MyClass类内部          within(MyClass)
+	10、位于Test类main方法调用的控制流中    cflow(call(void Test.main()))
+*/
+
 call(void Point.setX(int)   //  定义对Point类的一参数setX函数的调用为切点
 call(void Point.setx(int)) || call(void Line.setP1(Point)  //  使用 && , || , ! 组合切点，各切点可以属于不同的类
+
+/*  切点的模糊匹配定义  */	
+call(void Figure.make*(..))         //  定义一个切点，Figure类/接口中以make开头的任意参数的方法，其返回值是void
+call(public * Figure.*(..))         //  定义一个切点，指向Figure类/接口的所有公共方法
+cflow(move())                       //  使用cflow定义切点，指向所有move切点开始调用和返回之间的点
+
+// 方法调用举例：此处指向Point类的setX和setY方法调用
+pointcut setter(): target(Point) && (call(void setX(int)) || call(void setY(int)));
+
+// 异常处理举例：此处指向MyClass内部IOException异常处理部分，注意是异常处理的地方不是异常抛出的地方
+pointcut ioHandler(): within(MyClass) && handler(IOException);
+
+// 所有调用Point对象上返回int类型的无参方法的地方
+pointcut a(): target(Point) && call(int *());
+
+// Line和Point类内部任意方法调用
+pointcut b(): call(* *(..)) && (within(Line) || within(Point));
+
+// 不限制调用点，int型一参数构造函数执行时
+pointcut c(): within(*) && execution(*.new(int));
+
+// 非Point对象的返回值为int的函数执行时
+pointcut d(): !this(Point) && call(int *(..));
+
+// 公有非静态函数执行时
+pointcut e(): execution(public !static * *(..));
 
 /* 自定义一个切点，可以使用move()表示一个切点 */
 pointcut move() :
@@ -9,12 +50,9 @@ pointcut move() :
 	call(void Point.setX(int))              ||
 	call(void Line.setP1(int));
 	
-/*  切点的模糊匹配定义  */	
-call(void Figure.make*(..))         //  定义一个切点，Figure类/接口中以make开头的任意参数的方法，其返回值是void
-call(public * Figure.*(..))         //  定义一个切点，指向Figure类/接口的所有公共方法
-cflow(move())                       //  使用cflow定义切点，指向所有move切点开始调用和返回之间的点
-
-/******************               Advice举例                  ******************/
+	
+                       /******************               Advice举例                  ******************/
+					   
 // before原语定义move()调用前的建言
 before(): move() {
 	Sysout.out.println("about to move");
@@ -51,7 +89,7 @@ aspect SimpleTracing {
     }
 }
 
-/******************               Inter-type declarations内部变量声明                  ******************/
+                    /******************               Inter-type declarations内部变量声明                  ******************/
 // 给已有类的新增内部变量，此处给Point新增observers变量，该变量用于存储监控Point变化的Screen对象
 // 这种监控关系完全不需要在Point类和Screen类里面编写相关处理代码，所有具体关联处理全部放入本Aspect定义
 aspect PointObserving {
@@ -78,7 +116,7 @@ aspect PointObserving {
     }
 }
 
-/******************               Aspects定义                  ******************/
+                        /******************               Aspects定义                  ******************/
 // aspect切面的实例化都由AspectJ处理，默认是单例的，因此可以有局部变量和非静态函数的使用
 aspect Logging {
     OutputStream logStream = System.err;
@@ -161,5 +199,30 @@ aspect PublicErrorLogging {
 	after() throwing (Error e):
         publicMethodCall() && !cflow(publicMethodCall()) {  //  排除本地方法的内部互相调用时多次记录日志的情况
 		log.write(e);
+	}
+}
+
+aspect FaultHandler {
+	private boolean Server.disabled = false;
+
+	private void reportFault() {
+		System.out.println("Failure! Please fix it.");
+	}
+
+	public static void fixServer(Server s) {
+		s.disabled = false;
+	}
+
+	/* 定义切点，此处指定操作目标类为Server，切点为Server类所有public方法  */
+	pointcut services(Server s): target(s) && call(public * *(..));
+
+	before(Server s): services(s) {
+		if (s.disabled) throw new DisabledException();
+	}
+
+	/*  在services切点抛出FaultException异常后调用 */
+	after(Server s) throwing (FaultException e): services(s) {
+		s.disabled = true;
+		reportFault();
 	}
 }
