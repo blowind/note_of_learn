203e1ab26e90257b821317b9c93cffd197fcf8b4
zhangxf <fengxing581@gmail.com>
2018-11-14 19:34:13 +0800

    MyBatis Generator代码生成器示例

---
+++ java开发相关/mybatis/mgb/java/com/zxf/mybatis/generator/SpringBootApp.java
@@ -0,0 +1,19 @@
+package com.zxf.mybatis.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: SpringBootApp
+ * @Description:
+ * @Author: ZhangXuefeng
+ * @Date: 2018/11/14 19:20
+ * @Version: 1.0
+ **/
+@SpringBootApplication
+public class SpringBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+}
